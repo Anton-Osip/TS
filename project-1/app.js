7679ac6db371b8bb910ea56aff5ed2c768ba17f2
Anton-Osip <osipchika@gmail.com>
2022-10-03 15:16:56 +0300

    024 Types или Interfaces_

--- project-1/app.js
+++ project-1/app.js
@@ -1,11 +1,5 @@
 "use strict";
-let user = {
-    name: 'asd',
-    age: 33,
-    skills: ['1', '2'],
-    roleId: 5,
-    createdAt: new Date(),
-    log(id) {
-        return 'ghjk';
-    },
+const user = {
+    name: 'fghj',
+    age: 5,
 };
