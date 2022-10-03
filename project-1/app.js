b0276b51a5de38a8ef5cdfe9de282466fbc394fb
Anton-Osip <osipchika@gmail.com>
2022-10-03 15:28:54 +0300

    025 Optional

--- project-1/app.js
+++ project-1/app.js
@@ -1,5 +1,19 @@
 "use strict";
 const user = {
-    name: 'fghj',
-    age: 5,
+    login: 'a@mail.com',
+    // password: 'gfhj', Опциональный пароль  Может быть а может и не быть
 };
+function multiply(a, b) {
+    if (!b) {
+        return a * a;
+    }
+    return a * b;
+}
+function testPass(user) {
+    var _a;
+    const t = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type; // может быть андефаин
+    const f = user.password.type; // точно пароль будет
+}
+function test(param) {
+    const t = param !== null && param !== void 0 ? param : multiply(5);
+}
--- project-1/app.ts
+++ project-1/app.ts
@@ -1,12 +1,32 @@
 interface User {
-	name: string
-}
-
-interface User {
-	age: number
+	login: string
+	password?: string
 }
 
 const user: User = {
-	name: 'fghj',
-	age: 5,
+	login: 'a@mail.com',
+	// password: 'gfhj', Опциональный пароль  Может быть а может и не быть
+}
+
+function multiply(a: number, b?: number): number {
+	if (!b) {
+		return a * a
+	}
+	return a * b
+}
+
+interface UserPro {
+	login: string
+	password?: {
+		type: 'primary' | 'secondary'
+	}
+}
+
+function testPass(user: UserPro) {
+	const t = user.password?.type // может быть андефаин
+	const f = user.password!.type // точно пароль будет
+}
+
+function test(param?: string) {
+	const t = param ?? multiply(5)
 }
