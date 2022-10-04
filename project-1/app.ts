a57a9805ec1f573ac89c3108c2bfd4c9b0473469
Anton-Osip <osipchika@gmail.com>
2022-10-04 12:39:47 +0300

    032 Type Guard

--- project-1/app.ts
+++ project-1/app.ts
@@ -1,14 +1,3 @@
-let a: number = 5
-let b: string = a.toString()
-
-let c: string = 'asdf'
-let d: number = +c
-let g: number = parseInt(c)
-
-let e: string = new String(a).valueOf()
-let Str: String = new String(a)
-let bool: boolean = new Boolean(a).valueOf()
-
 interface User {
 	name: string
 	email: string
@@ -21,34 +10,35 @@ const user: User = {
 	login: 'vas',
 }
 
-const user1 = {
-	name: 'Vasya',
-	email: 'v@mail.ru',
-	login: 'vas',
-} as User
-
-const user3 = <User>{
-	name: 'Vasya',
-	email: 'v@mail.ru',
-	login: 'vas',
-}
-//не рекоминдуют использовать
-
 interface Admin {
 	name: string
 	role: number
 }
-//Преобразование
-//получили из пользователя админа
-const admin: Admin = {
-	...user,
-	role: 1,
+
+function isString(x: string | number): x is string {
+	return typeof x === 'string'
 }
 
-//ДЕЛАТ ТАК ФУНКЦИЯ MAPING
-function userToAdmin(user: User): Admin {
-	return {
-		name: user.name,
-		role: 1,
+function logId(id: string | number) {
+	if (isString(id)) {
+		console.log(id)
+	} else {
+		console.log(id)
+	}
+}
+
+function isAdmin(user: User | Admin): user is Admin {
+	return 'role' in user
+}
+
+function isAdminAlternative(user: User | Admin): user is Admin {
+	return (user as Admin).role !== undefined
+}
+
+function setRoleZero(user: User | Admin) {
+	if (isAdmin(user)) {
+		user.role = 0
+	} else {
+		throw new Error('Пользователь не админ')
 	}
 }
