38133a237d171d3ddfe82191bc16898a533d078e
Anton-Osip <osipchika@gmail.com>
2022-10-11 16:44:59 +0300

    045 Упражнение - Делаем корзину товаров

--- project-1/app.js
+++ project-1/app.js
@@ -1,18 +1,60 @@
 "use strict";
-class User {
-    constructor(name) {
+// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
+class Product {
+    constructor(id, price, name) {
+        this.id = id;
+        this.price = price;
         this.name = name;
     }
 }
-class Users extends Array {
-    searchByName(name) {
-        return this.filter(u => u.name === name);
-    }
-    toString() {
-        return this.map(u => u.name).join(', ');
+class Delivery {
+    constructor(date) {
+        this.date = date;
     }
 }
-const users = new Users();
-users.push(new User('vasya'));
-users.push(new User('Petya'));
-console.log(users.toString());
+class HomeDelivery extends Delivery {
+    constructor(date, adress) {
+        super(date);
+        this.adress = adress;
+    }
+}
+class ShopDelivery extends Delivery {
+    constructor(shopId) {
+        super(new Date());
+        this.shopId = shopId;
+    }
+}
+class Cart {
+    constructor() {
+        this.products = [];
+    }
+    addProduct(product) {
+        this.products.push(product);
+    }
+    removeProducts(productId) {
+        this.products = this.products.filter((p) => p.id !== productId);
+    }
+    getSum() {
+        return this.products.map((p) => p.price).reduce((p1, p2) => p1 + p2);
+    }
+    setDelivery(delivery) {
+        this.delivery = delivery;
+    }
+    checkout() {
+        if (this.products.length == 0) {
+            throw new Error('Нет товара в корзине');
+        }
+        if (!this.delivery) {
+            throw new Error('не указан способ доставки');
+        }
+        return { success: true };
+    }
+}
+const cart = new Cart();
+cart.addProduct(new Product(1, 10, 'xa'));
+cart.addProduct(new Product(2, 20, ',kf'));
+cart.addProduct(new Product(3, 60, 'ff'));
+cart.removeProducts(2);
+// cart.setDelivery(new HomeDelivery(new Date(), ' sdf'))
+console.log(cart.getSum());
+console.log(cart.checkout());
--- project-1/app.ts
+++ project-1/app.ts
@@ -1,30 +1,64 @@
-class Vehical {
-	public make: string //метод публчный
+// Delivery: может быть как до дома (дата и адрес) или до пункта выдачи (дата = Сегодня и Id магазина)
 
-	private demages: string[] //приватный метод или свойство доступно только внутри
-	private _model: string
+class Product {
+	constructor(public id: number, public price: number, public name: string) {}
+}
 
-	protected run: number //жоступны в наследуемых классах но не доступны снаружи
+class Delivery {
+	constructor(public date: Date) {}
+}
 
-	#price:number//==private
-
-	set model(model: string) {
-		this._model = model
-	}
-
-	get model() {
-		return this._model
-	}
-
-	addDemage(demage: string) {
-		this.demages.push(demage)
+class HomeDelivery extends Delivery {
+	constructor(date: Date, public adress: string) {
+		super(date)
 	}
 }
 
-class EuroTruck extends Vehical {
-	setRun(km: number) {
-		this.run = km / 0.62
+class ShopDelivery extends Delivery {
+	constructor(public shopId: number) {
+		super(new Date())
+	}
+}
+type DeliveryOptions = HomeDelivery | ShopDelivery
+
+class Cart {
+	private products: Product[] = []
+	private delivery: DeliveryOptions
+
+	public addProduct(product: Product): void {
+		this.products.push(product)
+	}
+
+	public removeProducts(productId: number): void {
+		this.products = this.products.filter((p: Product) => p.id !== productId)
+	}
+
+	public getSum(): number {
+		return this.products.map((p: Product) => p.price).reduce((p1: number, p2: number) => p1 + p2)
+	}
+	public setDelivery(delivery: DeliveryOptions): void {
+		this.delivery = delivery
+	}
+	public checkout() {
+		if (this.products.length == 0) {
+			throw new Error('Нет товара в корзине')
+		}
+		if (!this.delivery) {
+			throw new Error('не указан способ доставки')
+		}
+		return { success: true }
 	}
 }
 
-new Vehical()
+const cart = new Cart()
+cart.addProduct(new Product(1, 10, 'xa'))
+cart.addProduct(new Product(2, 20, ',kf'))
+cart.addProduct(new Product(3, 60, 'ff'))
+
+cart.removeProducts(2)
+
+// cart.setDelivery(new HomeDelivery(new Date(), ' sdf'))
+
+console.log(cart.getSum())
+
+console.log(cart.checkout())
